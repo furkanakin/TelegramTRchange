8fec7e3a59e12e7f90461ca20c3d055c9c19743f
Furkan <furkanakin1903@gmail.com>
2026-01-08 20:50:25 +0300

    Guncellendi: baslat.bat artik hatalari gosteriyor ve terminali acik tutuyor

--- baslat.bat
+++ baslat.bat
@@ -1,4 +1,10 @@
 @echo off
 echo Uygulama baslatiliyor...
+echo Moduller kontrol ediliyor...
 npm run dev
+if %errorlevel% neq 0 (
+    echo.
+    echo Bir hata olustu! Lutfen yukaridaki hatayi kontrol edin.
+    pause
+)
 pause
